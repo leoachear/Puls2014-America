71a73aed28f667564a3b387cfbd94d24705027b4
leoachear <leoachear@gmail.com>
2014-06-07 18:35:46 -0300

    Clase 5
    AJAX, CICLOS Y CONDICIONES: APLICANDO LO APRENDIDO

--- formulario.js
+++ formulario.js
@@ -1,15 +1,30 @@
-var $form = $('#formulario'),
-	$titulo = $('#titulo'),
-	$url = $('#link'),
+var $form 		= $('#formulario'),
+	$titulo 	= $('#titulo'),
+	$url 		= $('#link'),
 	$primerPost = $('.item').first()
-	$lista = $('#contenido');
+	$lista 		= $('#contenido'),
+	ss 			= sessionStorage;
+	ls 			= localStorage;
+
+if (ls.getItem('autosave')) {
+	$titulo.val(ss.getItem('titulo'));
+	$url.val(ss.getItem('url'));
+}
+
+var id = setInterval(function(){
+	ss.setItem('titulo', $titulo.val());
+	ss.setItem('url', $url.val());
+}, 1000);
 
 function mostrarOcultarFormulario(){
 	$form.slideToggle();
+	$lista.slideToggle();
 	return false;
 }
 
-function agregarPost(){
+function agregarPost(e){
+	e.preventDefault(); //reemplaza el return false que no se usa más.
+
 	var titulo = $titulo.val(),
 		url = $url.val(),
 		$clone = $primerPost.clone();
@@ -22,16 +37,43 @@ function agregarPost(){
 	
 	$lista.prepend($clone);
 
+	mostrarOcultarFormulario();
+	$titulo.val("");
+	$url.val("");
 	// este no se ve porque el primer <article> tiene el attributo de CSS min-height: 128px;
 	// sacándole este atributo desde CSS funciona.  Este es uno de los atributos que no se puede tocar mucho con JavaScript.
-	$clone.slideDown();
+	$clone.fadeIn();
 	//$clone.fadeIn(); //este es otro efecto.
 
-	return false; //esto sirve para que no recargue la página, las funciones que están escuchando "mas arriba" van a recibir false.
+/*	
+	Esto lo sacamos porque no se usa más, lo reemplaza el e.preventDefault()
+return false; //esto sirve para que no recargue la página, las funciones que están escuchando "mas arriba" van a recibir false.*/
+
 
 	// Esta instrucción la detecta la consola, y se frema acá cuando se esté ejecutando
 	// debugger;
 }
 
+function grabarInformacion(e){
+	e.preventDefault();
+
+	var titulo 	= $titulo.val(),
+		url 	= $url.val(),
+		ls 		= localStorage,
+		ss 		= sessionStorage;
+
+	ls.setItem('titulo', titulo);
+	ls.setItem('url', url);
+
+	ss.setItem('titulo', titulo);
+	ss.setItem('url', url);
+
+	mostrarOcultarFormulario();
+	$titulo.val("");
+	$url.val("");
+
+}
+
 $('#publicar_nav a').click(mostrarOcultarFormulario);
-$form.on('submit',agregarPost)
+/*$form.on('submit',grabarInformacion);*/
+$form.on('submit',agregarPost);
